7a16c3b89079bfd82be8736296fdb8aa524dd3ac
kbui1993 <kbui1993@gmail.com>
2014-12-02 21:46:48 -0800

    texture

--- Project/Code/project.js
+++ Project/Code/project.js
@@ -1,6 +1,12 @@
+//setting up web GL variables
 var canvas;
 var gl;
+
+//initializing color variable
 var vColor;
+
+//initializing buffers
+var vBuffer, tBuffer;
 
 // matrix transformation variables
 var ctm, modelViewMatrix, projectionMatrix;
@@ -51,88 +57,78 @@ var snd = new Audio("Ding.wav");
 var music = new Audio("GrapeGarden.mp3");
 var pop = new Audio("pop.wav");
 
-// keyboard controls
-document.addEventListener('keydown', function(event) {
-    switch(event.keyCode) {
-        case 32: // space - shoot bubble
-            snd.play();
-            fire();
-            snd.currentTime = 0;
-            break;
-        case 39: // right arrow
-            if (cannonAngle > -85)
-                cannonAngle -= 3;
-            break;
-        case 37: // left arrow
-        	if (cannonAngle < 85)
-                cannonAngle += 3;
-            break;
-        case 80: // p - pause game
-            pause = !pause;
-            if (pause)
-                music.pause();
-            else
-                music.play();
-            break;
-        case 83: // skip color
-            currBubble.color = nextBubble.color;
-            nextBubble.color = colors[Math.floor(Math.random() * colors.length)];
-            break;
-        case 82: // r - restart game
-            for(var i = 0; i < maxRows; i++) {
-                playingField[i] = undefined;
-            }
-            numRows = 0;
-            score = 0;
-            level = 1;
-            addRow();
-            break;
-    }
-});
+//intializing texture variables
+var texture;
+var texCoordsArray = [];
+var texSphere = [];
 
+var texCoord = [
+    vec2(0,0),
+    vec2(0,1),
+    vec2(1,1),
+    vec2(1,0)
+];
+
+//main function
 window.onload = function init() {
-	canvas = document.getElementById("gl-canvas");
-	gl = WebGLUtils.setupWebGL(canvas);
-	if(!gl){alert("WebGL isn't available");}
 
-	gl.viewport(0,0, canvas.width, canvas.height);
-	gl.clearColor(0.8, 0.9, 1.0, 1);
-	gl.enable(gl.DEPTH_TEST);
+    //setting up canvas
+    canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if(!gl){alert("WebGL isn't available");}
 
-	program = initShaders(gl, "vertex-shader", "fragment-shader");
-	gl.useProgram(program);
+    gl.viewport(0,0, canvas.width, canvas.height);
+    gl.clearColor(0.8, 0.9, 1.0, 1);
+    gl.enable(gl.DEPTH_TEST);
 
+    //setting up shader
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    //play music
     music.play();
 
-	// generate vertices
-	cannon();
-	tetrahedron(va, vb, vc, vd, 3);
+    // generate vertices
+    cannon();
+    textureCube(texCoordsArray, texCoord);
+    textureSphere();
+    tetrahedron(va, vb, vc, vd, 3);
 
-	// add new row every time interval
-	addRow();
+    // add new row every time interval
+    addRow();
     timer = window.setInterval(addRow, initialInterval);
-	
-	// link vColor on js to html
-	vColor = gl.getUniformLocation(program, "vColor");
+    
+    // link vColor on js to html
+    vColor = gl.getUniformLocation(program, "vColor");
 
-	// create buffer for points
-	var vBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    // create buffer for points
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    //gl.bufferData(gl.ARRAY_BUFFER, flatten(cannonPts), gl.STATIC_DRAW);
+    
+    //link vPosition from js to html
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+    
+    //create texture buffer
+    tBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    
+    //linking vTexCoord from js to html
+    var vTexCoord = gl.getAttribLocation(program, "vTexCoord");
+    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vTexCoord);
 
-	// link vPosition on js to html
-	var vPosition = gl.getAttribLocation(program, "vPosition");
-	gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(vPosition);
+    // link transformation matrices on js to html
+    modelViewMatrix = gl.getUniformLocation(program, "modelViewMatrix");
+    projectionMatrix = gl.getUniformLocation(program, "projectionMatrix");
 
-	// link transformation matrices on js to html
-	modelViewMatrix = gl.getUniformLocation(program, "modelViewMatrix");
-	projectionMatrix = gl.getUniformLocation(program, "projectionMatrix");
-
-	render();
+    setInterval(render,10);
 }
 
 function render() {
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // music looping
     if (typeof music.loop == 'boolean') {
@@ -146,17 +142,32 @@ function render() {
         music.play();
     }
 
-	// set up camera view
-	pMatrix = ortho(-10, 10, 0, 20, -10, 10);
-	gl.uniformMatrix4fv(projectionMatrix, false, flatten(pMatrix));
-
-	ctm = mat4();
+    // set up camera view
+    pMatrix = ortho(-10, 10, 0, 20, -10, 10);
+    gl.uniformMatrix4fv(projectionMatrix, false, flatten(pMatrix));
+    ctm = mat4();
     ctm = mult(ctm, rotate(cannonAngle,[0,0,1]));
-	// render cannon
-	gl.uniform4fv(vColor, cannonColor);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(cannonPts), gl.STATIC_DRAW);
-	gl.uniformMatrix4fv(modelViewMatrix, false, flatten(ctm));
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, numVertices);
+
+
+    // render cannon
+    gl.uniform4fv(vColor, cannonColor);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(cannonPts), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+    var image1 = document.getElementById("texImage");
+    configureTexture1(image1);
+
+    gl.uniformMatrix4fv(modelViewMatrix, false, flatten(ctm));
+    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(cannonPts), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+    var image2 = document.getElementById("texImage2");
+    configureTexture1(image2);
+
 
     //render line
     var dx = 0, dy = 0;
@@ -185,16 +196,22 @@ function render() {
         gl.uniformMatrix4fv(modelViewMatrix, false, flatten(ctm));
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, numVertices);
     }
+    
+    // render playing field
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(bubblePts), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texSphere), gl.STATIC_DRAW);
+    var image1 = document.getElementById("texImage");
+    configureTexture1( image1 );
 
-	// render playing field
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(bubblePts), gl.STATIC_DRAW);
-	for (var j = 0; j < numRows; j++) {
-		for (var k = 0; k < playingField[j].length; k++) {
-			drawBubble(playingField[j][k]);
+    for (var j = 0; j < numRows; j++) {
+        for (var k = 0; k < playingField[j].length; k++) {
+            drawBubble(playingField[j][k]);
             playingField[j][k].match = false;
             playingField[j][k].connected = false;
-		}
-	}
+        }
+    }
 
     // increment ball slowly so it is drawn when it hits the side
     for (var i = 0; i < 10; i++) {
@@ -302,8 +319,6 @@ function render() {
     // display level and score
     document.getElementById('level').innerHTML = "LEVEL " + level + ": ";
     document.getElementById('score').innerHTML = (Array(scoreLength).join("0") + score).slice(-scoreLength);
-
-	window.requestAnimFrame(render);
 }
 
 // Game functions
@@ -322,8 +337,34 @@ function updateScore() {
     }
 }
 
-// Functions for generating sphere vertices
+// Functions for generating cube vertices
+function cannon() {
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
+}
 
+function quad(a, b, c, d) {
+     var vertices = [vec4(-1, -1, 0, 1),
+                     vec4(-1,  2, 0, 1),
+                     vec4( 1,  2, 0 ,1),
+                     vec4( 1, -1, 0, 1),
+                     vec4(-1, -1, 0, 1),
+                     vec4(-1,  2, 0, 1),
+                     vec4( 1,  2, 0 ,1),
+                     vec4( 1, -1, 0 ,1)];
+
+    var indices = [a, b, c, a, c, d];
+
+    for (var i = 0; i < indices.length; ++i) {
+        cannonPts.push(vertices[indices[i]]);
+    }
+}
+
+// Functions for generating sphere vertices
 function triangle(a, b, c) {
      bubblePts.push(a);
      bubblePts.push(b);
@@ -359,29 +400,98 @@ function tetrahedron(a, b, c, d, n) {
     divideTriangle(a, c, d, n);
 }
 
-// Functions for generating cube vertices
-function cannon() {
-	quad(1, 0, 3, 2);
-	quad(2, 3, 7, 6);
-	quad(3, 0, 4, 7);
-	quad(6, 5, 1, 2);
-	quad(4, 5, 6, 7);
-	quad(5, 4, 0, 1);
+//configuring texture function
+function configureTexture1( image ) {
+    texture = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, 
+         gl.RGB, gl.UNSIGNED_BYTE, image );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, 
+                      gl.NEAREST );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    
+    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
 }
 
-function quad(a, b, c, d) {
-	 var vertices = [vec4(-1, -1, 0, 1),
-    				 vec4(-1,  2, 0, 1),
-        			 vec4( 1,  2, 0 ,1),
-        			 vec4( 1, -1, 0, 1),
-        			 vec4(-1, -1, 0, 1),
-        			 vec4(-1,  2, 0, 1),
-        			 vec4( 1,  2, 0 ,1),
-        			 vec4( 1, -1, 0 ,1)];
+//function that push vertices for texture cube
+function texturequad(texture_array, texture_coord)
+{
+    texture_array.push(texture_coord[0]);
+    texture_array.push(texture_coord[1]);
+    texture_array.push(texture_coord[2]); 
+    texture_array.push(texture_coord[0]); 
+    texture_array.push(texture_coord[2]);  
+    texture_array.push(texture_coord[3]); 
 
-	var indices = [a, b, c, a, c, d];
+}
 
-    for (var i = 0; i < indices.length; ++i) {
-        cannonPts.push(vertices[indices[i]]);
+//function that creates vertices for texture cube
+function textureCube(texture_array, texture_coord)
+{
+    texturequad(texture_array, texture_coord);
+    texturequad(texture_array, texture_coord);
+    texturequad(texture_array, texture_coord);
+    texturequad(texture_array, texture_coord);
+    texturequad(texture_array, texture_coord);
+    texturequad(texture_array, texture_coord);
+}
+
+//generating texture coordinates for sphere
+function textureSphere()
+{
+    var latitudeBands = 40;
+    var longitudeBands = 40;
+
+    for(var latNumber = 0; latNumber <= latitudeBands; latNumber++)
+    {
+        for(var longNumber = 0; longNumber <= longitudeBands; longNumber++)
+        {
+            var u = 1 - (longNumber/longitudeBands);
+            var v = 1 - (latNumber/latitudeBands);
+            texSphere.push(u);
+            texSphere.push(v);
+        }
     }
 }
+
+// keyboard controls
+document.addEventListener('keydown', function(event) {
+    switch(event.keyCode) {
+        case 32: // space - shoot bubble
+            snd.play();
+            fire();
+            snd.currentTime = 0;
+            break;
+        case 39: // right arrow
+            if (cannonAngle > -85)
+                cannonAngle -= 3;
+            break;
+        case 37: // left arrow
+            if (cannonAngle < 85)
+                cannonAngle += 3;
+            break;
+        case 80: // p - pause game
+            pause = !pause;
+            if (pause)
+                music.pause();
+            else
+                music.play();
+            break;
+        case 83: // skip color
+            currBubble.color = nextBubble.color;
+            nextBubble.color = colors[Math.floor(Math.random() * colors.length)];
+            break;
+        case 82: // r - restart game
+            for(var i = 0; i < maxRows; i++) {
+                playingField[i] = undefined;
+            }
+            numRows = 0;
+            score = 0;
+            level = 1;
+            addRow();
+            break;
+    }
+});
